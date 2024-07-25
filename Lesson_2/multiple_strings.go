033c4d35985d361bb68a5724267db0c0ae9aff00
Muhammad Tassadaq Abid <tassadaqq.abid@gmail.com>
2024-07-25 12:05:14 +0500

    Create multiple_strings.go

---
+++ Lesson_2/multiple_strings.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var firstname, middlename, lastname string = "Muhammad", "Tassadaq", "Abid"
+	fmt.Println("My full Name is" + firstname + middlename + lastname)
+}
