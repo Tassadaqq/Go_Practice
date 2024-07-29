21818135ebad885de60d4438784c111493124a22
Muhammad Tassadaq Abid <tassadaqq.abid@gmail.com>
2024-07-29 15:59:30 +0500

    Create function-in-function.go

---
+++ Lesson_21/function-in-function.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 4
+	b := 8
+	result := add(a, b)
+	fmt.Println(result)
+}
+func add(a int, b int) int {
+	c := a + b
+
+	c = a + b + 54
+	return c
+}
+func addten(ivinta hot nanh a;ko
+	a int,b int int) int,int,int  {
+	c += 10
+
+	c = a + b + 54
+	return c
+}
